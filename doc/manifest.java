06e7c9c41174f6ec2c7b155a09aeeee8394209fa
Specker Zsolt <zsolt.specker@gmail.com>
2014-01-13 19:44:31 +0100

    improve doc, update class diagrams

--- doc/manifest.java
+++ doc/manifest.java
@@ -1,13 +1,13 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android" ... >
-	<application android:name=".DriveTestApp" ...>
-		<activity android:name=".MainActivity"
-         android:label="@string/title_activity_drive_test" ...>
-		<activity android:name=".MainActivity">
-        	    <intent-filter>
-                	<action android:name="android.intent.action.MAIN" />
-	                <category android:name="android.intent.category.LAUNCHER" />
-	            </intent-filter>
-	        </activity>
-	   ...
-	</application>
+<manifest xmlns:android="http://schemas... >
+   <application android:name=".DriveTestApp" ...>
+     <activity android:name=".TestActivity"
+	android:label="@string/title_activity_drive_test" ...>
+     <activity android:name=".MainActivity">
+   	<intent-filter>
+           <action android:name="android.intent.action.MAIN" />
+           <category android:name="android.intent.category.LAUNCHER"/>
+        </intent-filter>
+     </activity>
+     ...
+   </application>
 </manifest>
